823a75ad1b9be789123c48fadffc019399740de8
ingecraft <ingenious.craftsman@gmail.com>
2020-03-26 19:34:30 +0200

    Added dairy report model

--- database/models.py
+++ database/models.py
@@ -1,3 +1,10 @@
 from .db import db
 
+class DailyEntry(db.Document):
+        date = db.DateTimeField(required=True)
+        country = db.StringField(required=True)
+        geo_id = db.StringField(required=True)
+        cases = db.IntegerField(required=True)
+        deaths = db.IntegerField(required=True)
+
 
