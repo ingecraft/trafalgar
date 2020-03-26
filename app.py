96fe945be9a52a2fa9cd3dd15f369e726c81f7d1
ingecraft <ingenious.craftsman@gmail.com>
2020-03-26 20:10:42 +0200

    Added CRUD for daily entries

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models import DailyEntry
 
 app = Flask(__name__)
 
-movies = [
-    {
-        "name": "The Shawshank Redemption",
-        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-        "genres": ["Drama"]
-    },
-    {
-       "name": "The Godfather ",
-       "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-       "genres": ["Crime", "Drama"]
-    }
-]
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/trafalgar'
+}
 
-@app.route('/movies')
-def hello():
-    return jsonify(movies)
+initialize_db(app)
 
+@app.route('/daily-entries')
+def get_daily_entries():
+    entries = DailyEntry.objects().to_json()
+    return Response(entries, mimetype="application/json", status=200)
+
+@app.route('/daily-entries', methods=['POST'])
+def add_daily_entry():
+    body = request.get_json()
+    entry = DailyEntry(**body).save()
+    id = entry.id
+    return {'id': str(id)}, 200
+
+@app.route('/daily_entries/<id>', methods=['PUT'])
+def update_movie(id):
+    body = request.get_json()
+    DailyEntry.objects.get(id=id).update(**body)
+    return '', 200
+
+@app.route('/daily_entries/<id>', methods=['DELETE'])
+def delete_entry(id):
+    entry = Entry.objects.get(id=id).delete()
+    return '', 200
+
+@app.route('/daily_entries/<id>')
+def get_daily_entry(id):
+    entry = DailyEntry.objects.get(id=id).to_json()
+    return Response(entry, mimetype="application/json", status=200)
+    
 app.run()
 
--- database/models.py
+++ database/models.py
@@ -4,7 +4,7 @@ class DailyEntry(db.Document):
         date = db.DateTimeField(required=True)
         country = db.StringField(required=True)
         geo_id = db.StringField(required=True)
-        cases = db.IntegerField(required=True)
-        deaths = db.IntegerField(required=True)
+        cases = db.IntField(required=True)
+        deaths = db.IntField(required=True)
 
 
