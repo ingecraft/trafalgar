fc4542008ab91ccc9c0c8bf16564ae78e1aa9dac
ingecraft <ingenious.craftsman@gmail.com>
2020-03-27 18:33:08 +0200

    Added api routes

--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,6 @@
 from .report import ReportApi, ReportsApi
 
 def initialize_routes(api):
-    api.add_resource(ReportsApi, '/reports')
-    api.add_resource(ReportApi, '/reports/<api>')
+    api.add_resource(ReportsApi, '/api/reports')
+    api.add_resource(ReportApi, '/api//reports/<id>')
+    
