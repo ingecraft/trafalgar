72e4b366d9a829bcfe7baa3647f1523e16bb5695
ingecraft <ingenious.craftsman@gmail.com>
2020-03-27 17:48:21 +0200

    Refactoring continued

--- resources/daily_report.py
+++ resources/daily_report.py
@@ -1,28 +1,33 @@
-@app.route('/daily-reports')
-def get_daily_reports():
-    reports = DailyReport.objects().to_json()
+from flask import Blueprint, Response, request
+from database.models import Report
+
+reports = Blueprint('reports', __name__)
+
+@app.route('/reports')
+def get_reports():
+    reports = Report.objects().to_json()
     return Response(reports, mimetype="application/json", status=200)
 
-@app.route('/daily-reports', methods=['POST'])
-def add_daily_report():
+@app.route('/reports', methods=['POST'])
+def add_report():
     body = request.get_json()
-    report = DailyReport(**body).save()
+    report = Report(**body).save()
     id = report.id
     return {'id': str(id)}, 200
 
-@app.route('/daily_reports/<id>', methods=['PUT'])
+@app.route('/reports/<id>', methods=['PUT'])
 def update_report(id):
     body = request.get_json()
-    DailyReport.objects.get(id=id).update(**body)
+    Report.objects.get(id=id).update(**body)
     return '', 200
 
-@app.route('/daily_reports/<id>', methods=['DELETE'])
+@app.route('/reports/<id>', methods=['DELETE'])
 def delete_report(id):
     report = Report.objects.get(id=id).delete()
     return '', 200
 
-@app.route('/daily_reports/<id>')
-def get_daily_report(id):
-    report = DailyReport.objects.get(id=id).to_json()
+@app.route('/reports/<id>')
+def get_report(id):
+    report = Report.objects.get(id=id).to_json()
     return Response(report, mimetype="application/json", status=200)
  
