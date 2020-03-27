3aa4d26c63ae910fe2848f66c89b5f50f82a7c9d
ingecraft <ingenious.craftsman@gmail.com>
2020-03-27 17:32:28 +0200

    Restructring started

--- database/models.py
+++ database/models.py
@@ -1,10 +1,11 @@
 from .db import db
 
 class DailyEntry(db.Document):
-        date = db.DateTimeField(required=True)
-        country = db.StringField(required=True)
-        geo_id = db.StringField(required=True)
-        cases = db.IntField(required=True)
-        deaths = db.IntField(required=True)
-
+    date = db.DateTimeField(required=True)
+    country = db.StringField(required=True)
+    geo_id = db.StringField(required=True)
+    country_code = db.StringField(required=True)
+    cases = db.IntField(required=True)
+    deaths = db.IntField(required=True) 
+        
 
