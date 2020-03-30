5ab2e5e4ad3ca6ceb8317ec6bff79d96ca3691ce
ingecraft <ingenious.craftsman@gmail.com>
2020-03-30 17:41:16 +0300

    Starting dockerization

--- app.py
+++ app.py
@@ -14,5 +14,12 @@ initialize_db(app)
    
 initialize_routes(api)
 
-app.run()
+@app.route('/')
+def hello():
+    return "Hello, World"
+
+if __name__== "__main__":
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
 
